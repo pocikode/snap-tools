4bd26f5725b4b37b43ab1d43d76c9626b4868e0b
Agus Supriyatna <agus@motionpay.id>
2025-02-05 18:12:26 +0700

    init project

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="drawer lg:drawer-open">
+    <NuxtLayout>
+      <input id="left-sidebar-drawer" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        <Navbar />
+        <main class="flex-1 h-screen overflow-y-auto md:py-4 py-4 px-6 bg-base-200">
+          <div class="min-h-[calc(100vh-250px)]">
+            <NuxtPage />
+          </div>
+        </main>
+      </div>
+      <Sidebar />
+
+    </NuxtLayout>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr: false,
+  telemetry: false,
+  css: ['~/assets/css/app.css'],
+  vite: {
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      strictPort: true,
+    },
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
