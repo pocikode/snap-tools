ec1b10219074ac652e25f8de80ce9c109d9216e2
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2025-02-09 17:31:42 +0700

    wip

--- pages/oauth/signature.vue
+++ pages/oauth/signature.vue
@@ -4,8 +4,8 @@ const isTimestampAuto = ref(true);
 
 <template>
   <h1 class="text-xl font-semibold">Generate OAuth Signature</h1>
-  <div class="flex flex-col">
-    <div class="flex mt-3">
+  <div class="flex flex-col gap-y-4 mt-4">
+    <div class="flex">
       <span class="shrink-1">Timestamp:</span>
       <div class="flex gap-x-2 pl-4">
         <input type="radio" name="timestamp_generate" id="timestamp-auto" :value="true" class="radio"
@@ -18,9 +18,14 @@ const isTimestampAuto = ref(true);
         <label for="timestamp-auto">Input Timestamp</label>
       </div>
     </div>
+
+    <Transition name="fade">
+      <label class="floating-label" v-if="!isTimestampAuto">
+        <span>Timestamp</span>
+        <input type="text" placeholder="2025-01-31 00:00:00+07" class="input input-md" />
+      </label>
+    </Transition>
+
+    <button type="button" class="btn btn-primary">Generate</button>
   </div>
-  <label class="floating-label">
-    <span>Timestamp</span>
-    <input type="text" placeholder="2025-01-31 00:00:00+07" class="input input-md" />
-  </label>
 </template>
